c4458669604bef333184c593715449be7c27338b
Alan Diaz <adiaz98.2020@gmail.com>
2021-11-29 21:03:42 -0500

    exit class

--- HotDog Or Not/ViewController.swift
+++ HotDog Or Not/ViewController.swift
@@ -6,14 +6,44 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDelegate, UINavigationControllerDelegate {
+    
+    // Delegation design pattern -> Text Field: Properties
+    
+    // Steps to implement a delegate:
+    // 1. Create an object
+    
+    let imagePicker = UIImagePickerController()
+    
+    let results = [VNClassificationObservation]()
+    //[0.8, 0.4, 0.3] -> 0.8 -> hotDog
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // 2. Initliaze the delegate in View Did load
+        imagePicker.delegate = self
     }
 
-
+    @IBAction func cameraPressed(_ sender: UIBarButtonItem) {
+        
+        imagePicker.sourceType = .camera
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        
+    }
+    
+    func detect(){
+        ßßßß
+    }
+    
 }
 
